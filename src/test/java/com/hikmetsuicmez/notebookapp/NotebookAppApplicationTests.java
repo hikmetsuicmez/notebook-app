d202142fe79d98c5286d562a82e0195c27d00176
Hikmet <hikmet.suicmez1@gmail.com>
2024-12-13 23:25:04 +0300

    chore: Initialize project with basic setup

---
+++ src/test/java/com/hikmetsuicmez/notebookapp/NotebookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hikmetsuicmez.notebookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookAppApplicationTests {
+
+        @Test
+        void contextLoads() {
+        }
+
+}
